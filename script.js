4e0ea5c47ac94bac9aaa8eda75fe5ecd95b55c22
Kilian-Stuardo <45419401+Kilian-Stuardo@users.noreply.github.com>
2020-05-26 15:59:33 +0200

    changes with background

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ var Bgtoggle = document.getElementById('Bgtoggle');
 Bgtoggle.addEventListener('click',function() {
     document.body.classList.toggle('Bgclass');
 });
+
+if (document.body.classList) {
+    setTimeout(() => {
+        document.body.classList.remove('Bgclass').delay(4000)
+    }, 20000);
+}
